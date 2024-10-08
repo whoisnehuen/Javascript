5ba10da7a98fe577747dd93866d2481a1af2b9df
whoisnehuen <lautarito222jara@gmail.com>
2024-10-08 20:37:08 -0300

    Create desafio5.js

---
+++ desafio5.js
@@ -0,0 +1,12 @@
+/*
+En este desafío, practicaremos el uso de arrays y bucles en JavaScript para gestionar productos en un e-commerce.
+
+Vamos a empezar creando un array de Productos:
+
+Para mostrar el contenido del array vamos a utilizar un “bucle for” para recorrer el mismo e imprimir en la consola cada uno de los productos.
+
+Ahora vamos a actualizar el stock:
+
+Utilizaremos otro “bucle for” para simular la venta de productos. Reduce el stock de productos eliminando el último con el método “pop” e imprime en la consola el nuevo stock después de la venta.
+*/
+
