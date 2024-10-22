95ece042187c1245d8892cab31c40acbcd9f3f8c
Nehuen Jara <lautarito222jara@gmail.com>
2024-10-21 22:59:24 -0300

    5.0

---
+++ desafio 7/app.js
@@ -0,0 +1,6 @@
+const precio = document.querySelector("#precioProducto");
+precio.innerHTML = "<span> Precio: $450 </span>";
+const titulo = document.querySelector("#nombreProducto");
+titulo.style.color = "Yellow";
+titulo.style.fontSize = "80px";
+titulo.style.fontFamily = "Playwrite GB S, cursive";
