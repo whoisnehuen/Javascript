dfaf8f91cc0bf8a1427246e347ab4357ac476e45
whoisnehuen <lautarito222jara@gmail.com>
2024-10-30 15:41:55 -0300

    6.1

--- desafio 8/main.js
+++ desafio 8/main.js
@@ -1,4 +1,5 @@
 let Procesador = {
+    nombre : "Procesador",
     marca : "Intel core",
     precio : 300000,
     modelo : "i5",
@@ -7,6 +8,7 @@ let Procesador = {
 }
 
 let Gabinete = {
+    nombre : "Gabinete",
     marca : "Sentey",
     precio : 60000,
     modelo : "office",
@@ -15,6 +17,7 @@ let Gabinete = {
 }
 
 let Motherboard = {
+    nombre : "Motherboard",
     marca : "MSI",
     precio : 150000,
     modelo : "B206",
@@ -23,6 +26,7 @@ let Motherboard = {
 }
 
 let Mouse = {
+    nombre : "Mouse",
     marca : "Genius",
     precio : 5600,
     modelo : "USB",
@@ -31,6 +35,7 @@ let Mouse = {
 }
 
 let Teclado = {
+    nombre : "Teclado",
     marca : "Genius",
     precio : 7800,
     modelo : "Bluetooth",
@@ -39,6 +44,7 @@ let Teclado = {
 }
 
 let Auriculares = {
+    nombre: "Auriculares",
     marca : "JBL",
     precio : 10000,
     modelo : "Bluetooth",
@@ -49,5 +55,34 @@ let Auriculares = {
 let Productos = [Procesador, Gabinete, Motherboard, Mouse, Teclado, Auriculares];
 
 for (i=0; i < Productos.length; i++){
-    console.log("Hola" + i);
+    let caja = document.querySelector("#contenedor");
+   
+    let titulo = document.createElement("h2");
+    titulo.innerText = Productos[i].nombre;
+    caja.appendChild(titulo);
+
+    let _marca = document.createElement("p");
+    _marca.innerText = "Marca : " + Productos[i].marca;
+    caja.appendChild(_marca);
+
+    let _modelo = document.createElement("p");
+    _modelo.innerText = "Modelo : " + Productos[i].modelo;
+    caja.appendChild(_modelo);
+
+    let _color = document.createElement("p");
+    _color.innerText = "Color : " + Productos[i].color;
+    caja.appendChild(_color);
+
+    let _precio = document.createElement("p");
+    _precio.innerText = "Precio : $" + Productos[i].precio;
+    caja.appendChild(_precio);
+
+    let _envio = document.createElement("p");
+    if (Productos[i].envioGratis == true){
+        _envio.innerText= "Con envio gratis!"
+    }
+    else{
+         _envio.innerText= "SIN envio gratis"
+    }
+    caja.appendChild(_envio);
 }
