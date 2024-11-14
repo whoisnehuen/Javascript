337d28ee28f9839b1935c980254b0597445aebdc
whoisnehuen <lautarito222jara@gmail.com>
2024-11-13 22:23:37 -0300

    10.5.1

--- desafio 9.5/main.js
+++ desafio 9.5/main.js
@@ -1,186 +1,87 @@
-let carrito = document.getElementById("shop");
-carrito.addEventListener("click", function(){
-    alert("Función no disponible :(");
+productos = [];
+
+const botonCarrito = document.getElementById('botonCarrito');
+const modal = document.getElementById('modal');
+const containerProductos = document.getElementById('productos');
+const botonParaCerrar = document.querySelector('.close');
+
+botonCarrito.addEventListener('click', () => {
+    updateCartDisplay();
+    modal.style.display = 'block';
 });
 
-let oferta = document.getElementById("ofertas");
-oferta.addEventListener("click", function(){
-    alert("Muy pronto, más novedades.");
+botonParaCerrar.addEventListener('click', () => {
+    modal.style.display = 'none';
 });
 
-
-let Procesador = {
-    nombre : "Procesador",
-    imagen: "./img/procesador.png",
-    marca : "Intel core",
-    precio : 302999,
-    cuotas : 6,
-    modelo : "i5",
-    color : "none",
-    envioGratis : true,
-}
-
-let Gabinete = {
-    nombre : "Gabinete",
-    imagen: "./img/gabinete.png",
-    marca : "Sentey",
-    precio : 41400,
-    cuotas : false,
-    modelo : "office",
-    color : "rojo",
-    envioGratis : true,
-}
-
-let Motherboard = {
-    nombre : "Motherboard",
-    imagen: "./img/mother.png",
-    marca : "MSI",
-    precio : 119999,
-    cuotas : 6,
-    modelo : "B206",
-    color : "gris",
-    envioGratis : true,
-}
-
-let Mouse = {
-    nombre : "Mouse",
-    imagen: "./img/mouse.png",
-    marca : "Genius",
-    precio : 5600,
-    cuotas : false,
-    modelo : "USB",
-    color : "rojo",
-    envioGratis : false,
-}
-
-let Teclado = {
-    nombre : "Teclado",
-    imagen: "./img/teclado.png",
-    marca : "Logitec",
-    precio : 46977.85,
-    cuotas : false,
-    modelo : "Bluetooth",
-    color : "rosa",
-    envioGratis : false,
-}
-
-let Auriculares = {
-    nombre: "Auriculares",
-    imagen: "./img/auricular.png",
-    marca : "JBL",
-    precio : 69992.22,
-    cuotas : 3,
-    modelo : "Bluetooth",
-    color : "azul",
-    envioGratis : false,
-}
-
-let Monitor = {
-    nombre: "Monitor",
-    imagen: "./img/monitor.png",
-    marca : "Samsung",
-    precio : 214999,
-    cuotas : 9,
-    modelo : "LED",
-    color : "black",
-    envioGratis : true,
-}
-
-let Smartphone = {
-    nombre: "Smartphone",
-    imagen: "./img/smartphone.png",
-    marca : "Samsung",
-    precio : 2599999,
-    cuotas : 12,
-    modelo : "S24 Ultra",
-    color : "grey",
-    envioGratis : true,
-}
-
-let Smartwatch = {
-    nombre: "Smartwatch",
-    imagen: "./img/smartwatch.png",
-    marca : "Samsung",
-    precio : 92159,
-    cuotas : false,
-    modelo : "Fit 3",
-    color : "black",
-    envioGratis : false,
-}
-
-let Impresora = {
-    nombre: "Impresora 3D",
-    imagen: "./img/impresora.webp",
-    marca : "Creality",
-    precio : 2599999,
-    cuotas : 9,
-    modelo : "Ender 3 V2",
-    envioGratis : true,
-}
-
-let Productos = [Procesador, Gabinete, Motherboard, Mouse, Teclado, Auriculares, Monitor, Smartphone, Smartwatch, Impresora];
-
-
-
-for (i=0; i < Productos.length; i++){
-    let caja = document.createElement("div");
-    caja.classList.add("caja-producto");
-
-    let titulo = document.createElement("h2");
-    titulo.innerText = Productos[i].nombre;
-    caja.appendChild(titulo);
-
-    let _imagen = document.createElement("img");
-    _imagen.src = Productos[i].imagen; // Asignar la imagen del producto
-    _imagen.alt = Productos[i].nombre; // Texto alternativo
-    caja.appendChild(_imagen);
-
-    let _marca = document.createElement("p");
-    _marca.innerText = "Marca : " + Productos[i].marca;
-    caja.appendChild(_marca);
-
-    let _modelo = document.createElement("p");
-    _modelo.innerText = "Modelo : " + Productos[i].modelo;
-    caja.appendChild(_modelo);
-
-    let _color = document.createElement("p");
-    _color.innerText = "Color : " + Productos[i].color;
-    caja.appendChild(_color);
-
-    let _precio = document.createElement("p");
-    _precio.innerText = "Precio : $" + Productos[i].precio;
-    caja.appendChild(_precio);
-
-    let _cuotas = document.createElement("p");
-    if (Productos[i].cuotas == false){
-        _cuotas.innerText = "Hasta 24 cuotas CON interés"
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
     }
-    else{
-        let precioCuota = Productos[i].precio / Productos[i].cuotas
-        _cuotas.innerText = "Hasta " + Productos[i].cuotas + " cuotas SIN interés de $" + precioCuota.toFixed(2)
-        _cuotas.style.color = "red";
-    }
-    caja.appendChild(_cuotas);
+});
 
-    let _envio = document.createElement("p");
-    if (Productos[i].envioGratis == true){
-        _envio.innerText= "Con envio gratis!"
+function addToCart(button, title, price) {
+    const existingItem = productos.find(item => item.title === title);
+    if (existingItem) {
+        existingItem.quantity++; 
+    } else {
+        productos.push({ title, price, quantity: 1 }); 
     }
-    else{
-         _envio.innerText= "SIN envio gratis"
-    }
-    caja.appendChild(_envio);
+    updateCartDisplay();
+    updateCartCount(); 
+    button.textContent = 'Agregado';
+    button.classList.add('added');
+}
 
-    let btn = document.createElement("button");
-    let txt = document.createElement("h3");
-    btn.innerText = "Agregar al carrito";
-    caja.appendChild(btn);
-    txt.innerText = "Producto agregado!";
-    txt.style.color = "green";
-
-    btn.addEventListener("click", function(){
-        caja.appendChild(txt);
+function updateCartDisplay() {
+    containerProductos.innerHTML = ''; 
+    let total = 0;
+    productos.forEach(item => {
+        const itemTotal = item.price * item.quantity;
+        total += itemTotal;
+        const cartItemElement = document.createElement('div');
+        cartItemElement.classList.add('cart-item');
+        cartItemElement.innerHTML = `
+            <p>${item.title} --> $${item.price} = $${itemTotal}</p>
+            <div class="quanty-controls">
+                <button class="quantity-btn" onclick="decrementQuantity('${item.title}')">-</button>
+                <span class="quantity">${item.quantity}</span>
+                <button class="quantity-btn" onclick="incrementQuantity('${item.title}')">+</button>
+            </div>
+            <button class="remove" onclick="removeFromCart('${item.title}')">Eliminar</button>
+        `;
+        containerProductos.appendChild(cartItemElement);
     });
+    const cartTotalElement = document.getElementById('carritoTotal');
+    cartTotalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
 
-    document.querySelector("#contenedor").appendChild(caja);
+function updateCartCount() {
+    const cartCountElement = document.getElementById('cantidadProductos');
+    const totalCount = productos.reduce((total, item) => total + item.quantity, 0);
+    cartCountElement.textContent = totalCount;
+}
+
+function removeFromCart(title) {
+    productos = productos.filter(item => item.title !== title);
+    updateCartDisplay();
+    updateCartCount();
+}
+
+function incrementQuantity(title) {
+    const item = productos.find(item => item.title === title);
+    if (item) {
+        item.quantity++;
+        updateCartDisplay();
+        updateCartCount();
+    }
+}
+
+function decrementQuantity(title) {
+    const item = productosCarrito.find(item => item.title === title);
+    if (item && item.quantity > 1) {
+        item.quantity--;
+        updateCartDisplay();
+        updateCartCount();
+    }
 }
